Lxiaoyao077/BreZygisk

improve: only compile Tango-related code for AArch64
This commit makes all Amenieu Systems' Tango-related code to only be built/included in AArch64 binaries/builds of ReZygisk, to reduce size.

loader/src/ptracer/utils.h
@@ -60,35 +60,38 @@ void align_stack(struct user_regs_struct *regs, long preserve);
 
 uintptr_t remote_call(int pid, struct user_regs_struct *regs, uintptr_t func_addr, uintptr_t return_addr, long *args, size_t args_size);
 
-long remote_syscall(int pid, struct user_regs_struct *regs, uintptr_t syscall_gadget, long sysnr, long *args, size_t args_size);
+int fork_dont_care();
 
 uintptr_t find_syscall_gadget(int pid, struct maps_info *remote_map);
 
-/* INFO: Tango-specific linker watch state */
-struct tango_linker_watch {
-  uint32_t libc_init_got_slot;
-  uint32_t libc_init_initial;
-  uint32_t libc_init_resolved;
-};
-
-bool ptrace_poke_u32(pid_t pid, uintptr_t addr, uint32_t value);
-
-uintptr_t find_arm32_ret_gadget(int pid, struct maps_info *remote_map);
-
-bool wait_for_ptrace_syscall_stop(int pid, int *status);
 bool wait_for_event_stop(int pid);
 
-bool tango_wait_linker_ready(int pid, struct tango_linker_watch *watch);
+#ifdef __aarch64__
+  /* INFO: Tango-specific linker watch state */
+  struct tango_linker_watch {
+    uint32_t libc_init_got_slot;
+    uint32_t libc_init_initial;
+    uint32_t libc_init_resolved;
+  };
 
-uint32_t find_tramp_padding(int pid, uint32_t rx_start, uint32_t rx_end, size_t needed);
+  bool tango_wait_linker_ready(int pid, struct tango_linker_watch *watch);
 
-int fork_dont_care();
+  uint32_t find_tramp_padding(int pid, uint32_t rx_start, uint32_t rx_end, size_t needed);
+
+  bool ptrace_poke_u32(pid_t pid, uintptr_t addr, uint32_t value);
+
+  uintptr_t find_arm32_ret_gadget(int pid, struct maps_info *remote_map);
+#endif
+
+bool wait_for_ptrace_syscall_stop(int pid, int *status);
+
+long remote_syscall(int pid, struct user_regs_struct *regs, uintptr_t syscall_gadget, long sysnr, long *args, size_t args_size);
+
+void tracee_skip_syscall(int pid);
 
 void wait_for_trace(int pid, int *status, int flags);
 
 void parse_status(int status, char *buf, size_t len);
-
-void tracee_skip_syscall(int pid);
 
 #define WPTEVENT(x) (x >> 16)
 

doubledashdot/NoZygisk

Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

loader/src/ptracer/utils.h
@@ -60,35 +60,38 @@ void align_stack(struct user_regs_struct *regs, long preserve);
 
 uintptr_t remote_call(int pid, struct user_regs_struct *regs, uintptr_t func_addr, uintptr_t return_addr, long *args, size_t args_size);
 
-long remote_syscall(int pid, struct user_regs_struct *regs, uintptr_t syscall_gadget, long sysnr, long *args, size_t args_size);
+int fork_dont_care();
 
 uintptr_t find_syscall_gadget(int pid, struct maps_info *remote_map);
 
-/* INFO: Tango-specific linker watch state */
-struct tango_linker_watch {
-	uint32_t libc_init_got_slot;
-	uint32_t libc_init_initial;
-	uint32_t libc_init_resolved;
-};
-
-bool ptrace_poke_u32(pid_t pid, uintptr_t addr, uint32_t value);
-
-uintptr_t find_arm32_ret_gadget(int pid, struct maps_info *remote_map);
-
-bool wait_for_ptrace_syscall_stop(int pid, int *status);
 bool wait_for_event_stop(int pid);
 
-bool tango_wait_linker_ready(int pid, struct tango_linker_watch *watch);
+#ifdef __arm__
+  /* INFO: Tango-specific linker watch state */
+  struct tango_linker_watch {
+    uint32_t libc_init_got_slot;
+    uint32_t libc_init_initial;
+    uint32_t libc_init_resolved;
+  };
 
-uint32_t find_tramp_padding(int pid, uint32_t rx_start, uint32_t rx_end, size_t needed);
+  bool tango_wait_linker_ready(int pid, struct tango_linker_watch *watch);
 
-int fork_dont_care();
+  uint32_t find_tramp_padding(int pid, uint32_t rx_start, uint32_t rx_end, size_t needed);
+
+  bool ptrace_poke_u32(pid_t pid, uintptr_t addr, uint32_t value);
+
+  uintptr_t find_arm32_ret_gadget(int pid, struct maps_info *remote_map);
+#endif
+
+bool wait_for_ptrace_syscall_stop(int pid, int *status);
+
+long remote_syscall(int pid, struct user_regs_struct *regs, uintptr_t syscall_gadget, long sysnr, long *args, size_t args_size);
+
+void tracee_skip_syscall(int pid);
 
 void wait_for_trace(int pid, int *status, int flags);
 
 void parse_status(int status, char *buf, size_t len);
-
-void tracee_skip_syscall(int pid);
 
 #define WPTEVENT(x) (x >> 16)
 